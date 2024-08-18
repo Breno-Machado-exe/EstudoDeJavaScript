ab935fac3c631adeeeedce384974334b685268c5
Bereno <burracha.6464@gmail.com>
2024-08-18 17:07:00 -0300

    adiciona a teoria de objetos com funções dentro

---
+++ A1.5 - Teoria com o Igor/01 - Aplicações/00 - objetos e funções/index.js
@@ -0,0 +1,24 @@
+
+// criação de um objeto
+
+let joao =
+{
+    fruta: "maçã",
+    quantidade: 1,
+    comeu: function()
+    {
+        joao.quantidade --;
+    }
+    //quanto tu chamar joao.comeu() a quantidade
+    //de maças dele diminui
+}
+let maria =
+{
+    fruta: "maçã",
+    quantidade: 2,
+    comeu: function()
+    {
+        maria.quantidade --;
+    }
+}
+
