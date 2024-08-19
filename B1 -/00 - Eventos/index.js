04263311e47482c84608c074aa9eb031dfebefad
Bereno <burracha.6464@gmail.com>
2024-08-19 00:59:49 -0300

    "finaliza" estudo de eventos

--- B1 -/00 - Eventos/index.js
+++ B1 -/00 - Eventos/index.js
@@ -34,6 +34,7 @@ function avisarQueTeclou ()
 {
     let numLetras = 1
     console.log('esta teclando' + ( letras.value.lenght + numLetras))
+    //nesse caso não temos textContent, pq não é texto. Temos valor, então usamos value
 }
 
 let botao1 = document.querySelector('#btn1')
@@ -42,16 +43,23 @@ let botaoEnviar = document.querySelector('#btn3')
 
 botao1.addEventListener('mouseover', function ()
 {
+    //percebe que ta só mouseover e não onmouseover
+    //isso pq está num ouvinte, então não precisa do on
     console.log('Foco no botão 1')
 })
 
 botao2.addEventListener('blur', function ()
 {
+    // onblur é quando perdemos o foco em um elemento
     console.log('Foco no botão 2')
 })
 
 botaoEnviar.addEventListener('click', function(event)
 {
+    //se tiver só o console,log, ao apertar em enviar a página vai dar um refresh
+    //pq é um elemento do type submit
+    //para evitar:
+    event.preventDefault()
     console.log('clicou no botão enviar')
 })
 
