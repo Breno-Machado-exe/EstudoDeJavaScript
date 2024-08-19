e0e18c4d14f9fafdfc03c5b90a9c6ddaae98d396
Bereno <burracha.6464@gmail.com>
2024-08-19 19:45:25 -0300

    Adiciona conteúdo de promise com alguns erros a serem resolvidos

--- B1 - Intermediario/01 - Promise/index.js
+++ B1 - Intermediario/01 - Promise/index.js
@@ -15,6 +15,8 @@ const minhaPromessa = new Promise((resolve,reject)=>{
 // a constante vai receber a promessa para que possamos trabalhar com 
 // seu resultado
 
+//por costume usamos arrow funcion para simplificar sua sintaxe
+
 // os parâmetros resolved e reject podem possuir outros nomes, mas por
 // convenção nós sempre usamos esses.
 
@@ -46,15 +48,6 @@ minhaPromessa
 //-----------------------------------------------------------------------------
 
 
-//              (retorno de constantes que receberam uma função)
-
-const pegaretorno = () =>{
-    return new Promise ((resolve,reject)=>{
-
-    })
-}
-
-
 //              (usa função externa)
 
 //caso já exista uma função externa, tu pode criar uma promise em uma variável
@@ -63,3 +56,122 @@ const pegaretorno = () =>{
 
 
 //-----------------------------------------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------
+
+//              (encadear os then)
+
+const myPromise = new Promise((resolve, reject) =>{
+
+    //resolve e reject são os dois argumentos dela 
+    const nome = "Breno"
+    // vamos fingir que esse nome chegou de uma requisição externa
+
+    if(nome === "Breno")
+    {
+        resolve('Usuário Breno encontrado!')
+    } else 
+    {
+        reject('O usuário Breno não foi encontrado!')
+        //envia mensaegem de erro
+    }
+
+})
+
+//agora vamos usar o then para executar algo graças a promessa 
+// e vamos encadear mais de um then
+
+myPromise.then((data) =>{
+    return data.toLowerCase()
+    //vai passar todas as letras para caixa baixa
+    //ou deveria, não está funcionando
+}).then((data)=>{
+    console.log(data)
+})
+//imprime com base no resultado do primeiro then da cadeia
+
+//----------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------
+
+//              (resolver várias promises com all)
+
+
+const p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('P1 ok! Timeout')
+    },2000)
+    //emulando uma demora de resposta para fins didáticos
+})
+const p2 = new Promise((resolve,reject)=>{
+    resolve('P2 ok!')
+})
+const p3 = new Promise((resolve,reject)=>{
+    resolve('P3 ok!')
+})
+
+const resolveAll = Promise.all([p1,p2,p3]).then((data)=>{
+    console.log(data)
+})
+
+//todas as respostas vem juntas, o método all prega que todas as promessas
+//tem que estar resolvidas para ele ser executado
+
+
+
+//              (resolver várias promises com o race)
+
+// a primeira a ser resolvida envia retorno
+
+
+const p4 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('P4 ok! Timeout')
+    },2000)
+    //emulando uma demora de resposta para fins didáticos
+})
+const p5 = new Promise((resolve,reject)=>{
+    resolve('P5 ok!')
+})
+const p6 = new Promise((resolve,reject)=>{
+    resolve('P6 ok!')
+})
+
+const resolveAllRace = Promise.race([p1,p2,p3]).then((data)=>{
+    console.log(data)
+})
+
+//não está funcionando também
+
+//----------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------
+
+// (promessas como retorno de função)
+
+function fezTarefa2()
+{
+    return new Promise((resolve,reject)=>{
+        setImmediate(()=>{
+            resolve("Tarefa 2")
+        })
+    })
+}
+
+console.log("Tarefa 1")
+fezTarefa2()
+    .then((valor)=>{
+        console.log(valor)
+    })
+    .then(()=>{
+        console.log("Tarefa 3")
+    })
+
+
+//----------------------------------------------------------------------
